6bfb8b42f2c573672e421e5f7a5373e69a6cfd5c
riyakulkarni837 <56438143+riyakulkarni837@users.noreply.github.com>
2024-02-27 12:23:43 -0800

    Add files via upload

--- fib.py
+++ fib.py
@@ -1,4 +1,9 @@
 # explanations for member functions are provided in requirements.py
+
+# 12041645
+# Shreya Chetan Pawaskar
+# pawaskas@uci.edu
+
 from __future__ import annotations
 
 class FibNode:
@@ -7,7 +12,6 @@ class FibNode:
         self.parent = None
         self.children = []
         self.flag = False
-        self.degree = 0
 
     def get_value_in_node(self):
         return self.val
@@ -21,120 +25,86 @@ class FibNode:
     def __eq__(self, other: FibNode):
         return self.val == other.val
 
-    def get_degree(self):
-        return self.degree
-
 class FibHeap:
     def __init__(self):
+        # Initialize a Fibonacci Heap with roots and the minimum node
         self.roots = []
-        self.min_node = None
+        self.min = None
 
     def get_roots(self) -> list:
         return self.roots
 
     def insert(self, val: int) -> FibNode:
         new_node = FibNode(val)
+        # Update the minimum node if needed
+        if self.min is None or self.min.val > val:
+            self.min = new_node
+        # Add the new node to the roots list
         self.roots.append(new_node)
-
-        # Updating the minimum node if necessary
-        if self.min_node is None or val < self.min_node.val:
-            self.min_node = new_node
         return new_node
-        
+
+    def update_min(self):
+        for node in self.roots:
+            if self.min is None or self.min.val > node.val:
+                self.min = node
+
     def delete_min(self) -> None:
-        if not self.min_node:
+        if not self.roots:
             return
 
-        self.roots.remove(self.min_node)
+        min_node = self.min
+        # Remove the minimum node from the roots list
+        self.roots.remove(min_node)
+        # Add the children of the minimum node back to the roots list
+        for child in min_node.children:
+            child.flag = False
+            child.parent = None
+            self.roots.append(child)
 
-        # Adding children of the minimum node to the root list.
-        if self.min_node.children:
-            for child in self.min_node.children:
-                self.roots.append(child)
-                child.parent = None
+        # Reorganize the heap to ensure at most one root node for each degree
+        degrees = {}
+        while len(self.roots) != 0:
+            current_node = self.roots.pop(0)
+            degree = len(current_node.children)
+            if degree not in degrees:
+                degrees[degree] = current_node
+            else:
+                existing_node = degrees[degree]
+                if current_node.val < existing_node.val:
+                    current_node.children.append(existing_node)
+                    existing_node.parent = current_node 
+                    self.roots.append(current_node)
+                else:
+                    existing_node.children.append(current_node)
+                    current_node.parent = existing_node
+                    self.roots.append(existing_node)
+                degrees.pop(degree)
 
-        self.consolidate_heap()
-        
-        # Updating the minimum node
-        if self.roots:
-            self.min_node = min(self.roots, key=lambda x: x.val)
-        else:
-            self.min_node = None
+        for node in degrees.values():
+            self.roots.append(node)
+        self.min = None
+        self.update_min()
 
     def find_min(self) -> FibNode:
-        return self.min_node
+        return self.min
 
     def decrease_priority(self, node: FibNode, new_val: int) -> None:
         node.val = new_val
-        parent = node.parent
+        # Perform a cut operation to promote the node to the root list
+        self.promote(node)
+        # Update the minimum node
+        self.update_min()
         
-        # Performing cascading cuts if necessary.
-        if parent and node.val < parent.val:
-            self.cut_node_from_parent(node, parent)
-            self.cascade_cut_node(parent)
-        
-        # Updating the minimum node
-        if node.val < self.min_node.val:
-            self.min_node = node
-
-    def consolidate_heap(self):
-        # Consolidating the root list to ensure no two trees have the same degree.
-        degree_array = [None] * (len(self.roots) * 2)
-        new_roots = []
-
-        for root in self.roots:
-            x = root
-            d = x.get_degree()
-            while degree_array[d] is not None:
-                y = degree_array[d]
-                if x.val > y.val:
-                    x, y = y, x
-                self.link_child_to_parent(y, x)
-                degree_array[d] = None
-                d += 1
-            degree_array[d] = x
-
-        self.roots = []
-        for node in degree_array:
-            if node is not None:
-                new_roots.append(node)
-
-        self.roots = new_roots
-
-    def cut_node_from_parent(self, child: FibNode, parent: FibNode):
-        # Cuting a child node from its parent and add it to the root list.
-        if child in parent.children:
-            parent.children.remove(child)
-            self.roots.append(child)
-            child.parent = None
-            child.flag = False
-
-        parent.degree -= 1
-
-    def cascade_cut_node(self, node: FibNode):
-        # Cascading cuts upward through the tree.
-        # Cutting and adding parent to the rootlist until root node or unmarked node is reached
-        parent = node.parent
-        if parent:
-            if not node.flag:
-                node.flag = True
-            else:
-                self.cut_node_from_parent(node, parent)
-                self.cascade_cut_node(parent)
-
-    def link_child_to_parent(self, y: FibNode, x: FibNode):
-        # Linking a child node to a parent node
-        self.roots.remove(y)
-
-        if x.children:
-            x.children.append(y)
-        else:
-            x.children = [y]
-
-        y.parent = x
-        y.flag = False
-        x.degree += 1
-
-    # feel free to define new methods in addition to the above
-    # fill in the definitions of each required member function (above),
-    # and for any additional member functions you define
+    def promote(self, node: FibNode) -> None:
+        # Cut a node and promote it to the root list
+        if node not in self.roots:
+            parent = node.parent
+            parent.children.remove(node)
+            node.flag = False
+            # Add the cut node to the roots list
+            self.roots.append(node)
+            # Recursively cut the parent if needed
+            if parent.flag:
+                self.promote(parent)
+            elif parent not in self.roots:
+                parent.flag = True
